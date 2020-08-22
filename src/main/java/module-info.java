5b56100e80bd204c9a6cf559e46563ccac996f5e
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-08-22 14:34:47 +0200

    Modularize project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -16,12 +16,9 @@
  * limitations under the License.
  */
 
-/**
- * The JCommander module declaration.
- */
-module com.beust.jcommander {
-    exports com.beust.jcommander; 
-    exports com.beust.jcommander.converters; 
-    exports com.beust.jcommander.defaultprovider; 
-    exports com.beust.jcommander.validators; 
+module bayern.steinbrecher.JCommander {
+    exports com.beust.jcommander;
+    exports com.beust.jcommander.converters;
+    exports com.beust.jcommander.defaultprovider;
+    exports com.beust.jcommander.validators;
 }
